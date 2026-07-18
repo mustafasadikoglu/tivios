ecd3d17da012d727516ab4ed9781f411dfcd55dc
mustafasadikoglu <66087800+mustafasadikoglu@users.noreply.github.com>
2026-07-18 23:08:19 +0300

    ci: redirect xcodebuild logs to stdout on failure for diagnostics

--- TiviOS/Sources/Presentation/Components/AirPlayPickerView.swift
+++ TiviOS/Sources/Presentation/Components/AirPlayPickerView.swift
@@ -1,13 +1,14 @@
 #if os(iOS)
 import SwiftUI
 import AVKit
+import UIKit
 
 public struct AirPlayPickerView: UIViewRepresentable {
     public init() {}
     
     public func makeUIView(context: Context) -> AVRoutePickerView {
         let picker = AVRoutePickerView()
-        picker.activeTintColor = UIColor(red: 255/255, green: 0/255, blue: 122/255, alpha: 1.0) // FF007A
+        picker.activeTintColor = UIColor(red: 255/255, green: 0/255, blue: 122/255, alpha: 1.0) // // FF007A
         picker.tintColor = .white
         return picker
     }
